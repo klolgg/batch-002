c9722d16d9b8e42316eb12b7f6c8a7842305bb26
seung-woo-ryu <tmddn645@naver.com>
2025-02-07 16:45:12 +0900

    init

---
+++ src/main/java/site/klol/batch002/Batch002Application.java
@@ -0,0 +1,13 @@
+package site.klol.batch002;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Batch002Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Batch002Application.class, args);
+	}
+
+}
---
+++ src/test/java/site/klol/batch002/Batch002ApplicationTests.java
@@ -0,0 +1,13 @@
+package site.klol.batch002;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Batch002ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
